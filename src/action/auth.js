2568db0176204f6f41b42a8ca6c3a02d947644ca
Jordy9 <xilero23@gmail.com>
2022-02-09 19:36:00 -0400

    Se pueden actualizar las contrasenas de usuario ahora mediante el backoofice

--- src/action/auth.js
+++ src/action/auth.js
@@ -46,7 +46,22 @@ export const startLogin = (email, password) => {
                 title: `${greet} ${body.name}`
               })             
         } else {
-            Swal.fire('Error', body.msg, 'error')
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 5000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              
+              return Toast.fire({
+                icon: 'error',
+                title: `${body.msg}`
+              })
         }
     }
 }
@@ -181,7 +196,7 @@ export const startUpdateUser = (name, lastName, age, date, email, address, count
               
               return Toast.fire({
                 icon: 'error',
-                title: `${body.errors.email.msg}`
+                title: `${body.msg}`
               })
         }
     }
@@ -228,7 +243,7 @@ export const startUpdateUserUsuario = (name, lastName, age, date, email, address
               
               return Toast.fire({
                 icon: 'error',
-                title: `${body.errors.email.msg}`
+                title: `${body.msg}`
               })
         }
     }
--- src/action/petition.js
+++ src/action/petition.js
@@ -158,7 +158,22 @@ export const startUpdatePetition = (title, date, descripcion) => {
                 title: 'Petición actualizada correctamente'
               })
         } else {
-            Swal.fire('Error', body.msg, 'error')
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 5000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              
+              return Toast.fire({
+                icon: 'error',
+                title: `${body.msg}`
+              })
         }
 
     }
