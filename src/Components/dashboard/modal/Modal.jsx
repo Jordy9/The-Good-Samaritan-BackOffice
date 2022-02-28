c8396b5e037ff7242c728c172a491bf176ad2894
Jordy9 <xilero23@gmail.com>
2022-02-27 23:36:26 -0400

    Mejoras

--- src/Components/dashboard/modal/Modal.jsx
+++ src/Components/dashboard/modal/Modal.jsx
@@ -107,89 +107,75 @@ export const Modal = () => {
                                 <h5 className="text-white text-center mt-2">Ver/Editar usuario</h5>
                                 <div className="card-body">
                                     <form onSubmit = {handleSubmit} className = 'needs-validation'>
-                                    <div className="row">
-                                        <div className="col form-group">
-                                            <label>Nombre</label>
-                                            <input type="text" {...getFieldProps('name')} placeholder = 'Juan' className = 'form-control bg-transparent text-white' />
-                                            {touched.name && errors.name && <span style={{color: 'red'}}>{errors.name}</span>}
+                                        <div className="row">
+                                            <div className="col form-group">
+                                                <label>Nombre</label>
+                                                <input type="text" {...getFieldProps('name')} placeholder = 'Juan' className = 'form-control bg-transparent text-white' />
+                                                {touched.name && errors.name && <span style={{color: 'red'}}>{errors.name}</span>}
+                                            </div>
+
+                                            <div className="col form-group">
+                                                <label>Apellido</label>
+                                                <input type="text" {...getFieldProps('lastName')} placeholder = 'Taveras' className = 'form-control bg-transparent text-white' />
+                                                {touched.lastName && errors.lastName && <span style={{color: 'red'}}>{errors.lastName}</span>}
+                                            </div>
+                                        </div>
+                                        
+                                        <div className="row">
+                                            <div className="col form-group">
+                                                <label>Edad</label>
+                                                <input type="number" {...getFieldProps('age')} placeholder = '25' className = 'form-control bg-transparent text-white' />
+                                                {touched.age && errors.age && <span style={{color: 'red'}}>{errors.age}</span>}
+                                            </div>
+
+                                            <div className="col form-group">
+                                                <label>Fecha de nacimiento</label>
+                                                <input type="date" min={newDate} {...getFieldProps('date')} placeholder = '26/8/1996' className = 'form-control bg-transparent text-white ' />
+                                                {touched.date && errors.date && <span style={{color: 'red'}}>{errors.date}</span>}
+                                            </div>
                                         </div>
 
-                                        <div className="col form-group">
-                                            <label>Apellido</label>
-                                            <input type="text" {...getFieldProps('lastName')} placeholder = 'Taveras' className = 'form-control bg-transparent text-white' />
-                                            {touched.lastName && errors.lastName && <span style={{color: 'red'}}>{errors.lastName}</span>}
-                                        </div>
-                                    </div>
-                                    
-                                    <div className="row">
-                                        <div className="col form-group">
-                                            <label>Edad</label>
-                                            <input type="number" {...getFieldProps('age')} placeholder = '25' className = 'form-control bg-transparent text-white' />
-                                            {touched.age && errors.age && <span style={{color: 'red'}}>{errors.age}</span>}
+                                        <div className="row">
+                                            <div className="col form-group">
+                                                <label>Correo Electrónico</label>
+                                                <input readOnly type="text" {...getFieldProps('email')} placeholder = 'Juan123@hotmail.com' className = 'form-control bg-transparent text-white ' />
+                                                {touched.email && errors.email && <span style={{color: 'red'}}>{errors.email}</span>}
+                                            </div>
                                         </div>
 
-                                        <div className="col form-group">
-                                            <label>Fecha de nacimiento</label>
-                                            <input type="date" min={newDate} {...getFieldProps('date')} placeholder = '26/8/1996' className = 'form-control bg-transparent text-white ' />
-                                            {touched.date && errors.date && <span style={{color: 'red'}}>{errors.date}</span>}
-                                        </div>
-                                    </div>
-
-                                    <div className="row">
-                                        <div className="col form-group">
-                                            <label>Correo Electrónico</label>
-                                            <input type="text" {...getFieldProps('email')} placeholder = 'Juan123@hotmail.com' className = 'form-control bg-transparent text-white ' />
-                                            {touched.email && errors.email && <span style={{color: 'red'}}>{errors.email}</span>}
-                                        </div>
-                                    </div>
-
-                                    <div className="row">
-                                        <div className="col form-group">
-                                            <label>Dirección</label>
-                                            <input type="text" {...getFieldProps('address')} placeholder = 'Los Santos' className = 'form-control bg-transparent text-white' />
-                                            {touched.address && errors.address && <span style={{color: 'red'}}>{errors.address}</span>}
-                                        </div>
-                                    </div>
-
-                                    <div className="row">
-                                        <div className="col form-group">
-                                            <label>País</label>
-                                            <input type="text" {...getFieldProps('country')} placeholder = 'República Dominicana' className = 'form-control bg-transparent text-white' />
-                                            {touched.country && errors.country && <span style={{color: 'red'}}>{errors.country}</span>}
+                                        <div className="row">
+                                            <div className="col form-group">
+                                                <label>Dirección</label>
+                                                <input type="text" {...getFieldProps('address')} placeholder = 'Los Santos' className = 'form-control bg-transparent text-white' />
+                                                {touched.address && errors.address && <span style={{color: 'red'}}>{errors.address}</span>}
+                                            </div>
                                         </div>
 
-                                        <div className="col form-group">
-                                            <label>Ciudad</label>
-                                            <input type="text" {...getFieldProps('city')} placeholder = 'Bonao' className = 'form-control bg-transparent text-white' />
-                                            {touched.city && errors.city && <span style={{color: 'red'}}>{errors.city}</span>}
+                                        <div className="row">
+                                            <div className="col form-group">
+                                                <label>País</label>
+                                                <input type="text" {...getFieldProps('country')} placeholder = 'República Dominicana' className = 'form-control bg-transparent text-white' />
+                                                {touched.country && errors.country && <span style={{color: 'red'}}>{errors.country}</span>}
+                                            </div>
+
+                                            <div className="col form-group">
+                                                <label>Ciudad</label>
+                                                <input type="text" {...getFieldProps('city')} placeholder = 'Bonao' className = 'form-control bg-transparent text-white' />
+                                                {touched.city && errors.city && <span style={{color: 'red'}}>{errors.city}</span>}
+                                            </div>
+
+                                            <div className="col form-group">
+                                                <label >Número de teléfono</label>
+                                                <MaskedInput
+                                                    {...getFieldProps('number')}
+                                                    className = 'form-control bg-transparent text-white'
+                                                    placeholder = '(809)-222-3333)'
+                                                    mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+                                                />
+                                                {touched.number && errors.number && <span style={{color: 'red'}}>{errors.number}</span>}
+                                            </div>
                                         </div>
 
-                                        <div className="col form-group">
-                                            <label >Número de teléfono</label>
-                                            <MaskedInput
-                                                {...getFieldProps('number')}
-                                                className = 'form-control bg-transparent text-white'
-                                                placeholder = '(809)-222-3333)'
-                                                mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-                                            />
-                                            {touched.number && errors.number && <span style={{color: 'red'}}>{errors.number}</span>}
-                                        </div>
-                                    </div>
-
-                                    <div className="row">
-                                        <div className="col form-group">
-                                            <label>Contrasena</label>
-                                            <input type="text" {...getFieldProps('password')} placeholder = '********' className = 'form-control bg-transparent text-white' />
-                                            {touched.password && errors.password && <span style={{color: 'red'}}>{errors.password}</span>}
-                                        </div>
-
-
-                                        <div className="col form-group">
-                                            <label>Confirmar Contrasena</label>
-                                            <input type="text" {...getFieldProps('confirmPassword')} placeholder = '********' className = 'form-control bg-transparent text-white' />
-                                            {touched.confirmPassword && errors.confirmPassword && <span style={{color: 'red'}}>{errors.confirmPassword}</span>}
-                                        </div>
-                                    </div>
                                         <button type='submit' className = 'btn btn-outline-primary form-control' data-bs-dismiss="modal" aria-label="Close">Guardar</button>
                                     </form>
                                 </div>
